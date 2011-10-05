e5dd5f5d71c8607ce5aefe18ba3e047b332c0532
tkawa <tkawa@4bit.net>
2011-10-05 14:41:29 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Funcan::Application.config.secret_token = '8c2336437f9b7ef0d1f64c71e470402a14491310d1f3ae53c092419f51333f78973159103850c17bc170f879aea5cc325b8a8dd3c143b12edb970628ec7d5738'
