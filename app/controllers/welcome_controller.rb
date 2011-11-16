58016fabe4d840c24fbf265d626e977db485f4dd
tkawa <tkawa@4bit.net>
2011-11-16 09:33:29 +0900

    show tweets(funcans) on the toppage when not logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     #@disp = Rails.application.config.middleware.middlewares.last.instance_variable_get(:@block) #instance_eval{|m| m.instance_variables.map{|s| [s, m.instance_variable_get(s)] } }
+    raw = Twitter.search('#funcan', include_entities: true)
+    @tweets = raw.map {|t| FuncanDelegator.new(t) }
   end
 end
