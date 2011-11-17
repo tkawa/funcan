9bdd32c545c881bca04ec8e2681a8bd12db01d06
tkawa <tkawa@4bit.net>
2011-11-17 11:26:58 +0900

    added emoji PNGs

--- app/helpers/funcans_helper.rb
+++ app/helpers/funcans_helper.rb
@@ -2,4 +2,12 @@ module FuncansHelper
   def signed_in?
     session[:user_id].present?
   end
+
+  def emoji(type, opt = nil)
+    if opt == :iphone
+      "&#x#{type.camelize.constantize.emoji_code}"
+    else
+      image_tag "#{type.camelize.constantize.emoji_code}.png", alt: '', class: 'emoji'
+    end
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -8,16 +8,20 @@ end
 
 class Like < Comment
   belongs_to :funcan, counter_cache: 'like_count'
+  def self.emoji_code; 'e00e' end
 end
 
 class Want < Comment
   belongs_to :funcan, counter_cache: 'want_count'
+  def self.emoji_code; 'e427' end
 end
 
 class Great < Comment
   belongs_to :funcan, counter_cache: 'great_count'
+  def self.emoji_code; 'e14c' end
 end
 
 class Interesting < Comment
   belongs_to :funcan, counter_cache: 'interesting_count'
+  def self.emoji_code; 'e41b' end
 end
